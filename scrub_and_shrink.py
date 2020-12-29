85b59b9c74f1521dbf6523751f7041fb953128d1
Nathan Day <nathancday@gmail.com>
2020-12-29 11:57:54 -0500

    adjusted filter settings to focus on English movies that have a few votes (mildly popular)

--- scrub_and_shrink.py
+++ scrub_and_shrink.py
@@ -30,7 +30,8 @@ def scrub_chunks():
             for m in movies.keys():
                 dat = movies[m]
                 if ( not dat["adult"] and
-                    dat["vote_count"] > 0 and
+                    dat["vote_count"] > 5 and
+                    dat["original_language"] == "en" and
                     dat["poster_path"] is not None and
                     dat["runtime"] is not None and
                     dat["runtime"] > 59 and
